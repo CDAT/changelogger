6d3ef3400899abb53484898a2e0e424e02d986a0
Samuel Fries <fries2@llnl.gov>
2015-11-18 13:54:18 -0800

    Updated labels and filtering logic

--- changelog.py
+++ changelog.py
@@ -6,18 +6,18 @@ repo = gh.GithubModel("/repos/UV-CDAT/uvcdat")
 milestones = repo["milestones"]
 
 for m in milestones:
-	if m["title"] == str(milestone):
-		break
+    if m["title"] == str(milestone):
+        break
 else:
-	print "Unable to find milestone %s" % milestone
-	sys.exit(1)
+    print "Unable to find milestone %s" % milestone
+    sys.exit(1)
 
 issues = gh.GithubModel("/repos/UV-CDAT/uvcdat/issues?milestone=%d&state=closed" % m["number"])
 
 labels = repo["labels"]
 
 # Used to sort open issues and stick at end of log
-severity = ["High", "Critical"]
+severity = ["High Priority", "Low Priority", "Critical"]
 
 # Used to split category closed issues 
 kind = ["Enhancement", "Bug"]
@@ -25,57 +25,71 @@ kind = ["Enhancement", "Bug"]
 category = []
 
 # Ignored
-irrelevant = ["Other", "Duplicate", "Invalid", "Question"]
+irrelevant = ["wontfix", "Other", "Duplicate", "Invalid", "Question", "Gatekeeper", "unconfirmed"]
+skip = ["wontfix", "Duplicate", "Invalid"]
 
 for label in labels:
-	if label["name"] in severity or label["name"] in kind or label["name"] in irrelevant:
-		continue
-	category.append(label["name"])
+    if label["name"] in severity or label["name"] in kind or label["name"] in irrelevant:
+        continue
+    category.append(label["name"])
 
 issues_sorted = {}
 issues_kinds = {}
 for issue in issues:
-	issue_labels = issue["labels"]
-	for label in issue_labels:
-		if label["name"] in category:
-			if label["name"] not in issues_sorted:
-				issues_sorted[label["name"]] = []
-			issues_sorted[label['name']].append(issue)
-		if label["name"] in kind:
-			issues_kinds[issue["url"]] = label["name"]
+    issue_labels = issue["labels"]
+    labels = [l["name"] for l in issue_labels]
+    if (any(s in labels for s in skip)):
+        continue
+    for l in labels:
+        if l in category:
+            if l not in issues_sorted:
+                issues_sorted[l] = []
+            issues_sorted[l].append(issue)
+        if l in kind:
+            issues_kinds[issue["url"]] = l
 
 print "## Closed Issues"
 
 categories = sorted(issues_sorted.keys())
 for cat in categories:
-	print "### {category}".format(category=cat)
-	print ""
-	for issue in issues_sorted[cat]:
-		values = {
-			"bug_or_enh": issues_kinds[issue["url"]] if issue["url"] in issues_kinds else "",
-			"title": issue["title"],
-			"url": issue["html_url"]
-		}
-		print " * **{bug_or_enh}**: [{title}]({url})".format(**values)
-	print ""
+    print "### {category}".format(category=cat)
+    print ""
+    for issue in issues_sorted[cat]:
+        values = {
+            "bug_or_enh": issues_kinds[issue["url"]] if issue["url"] in issues_kinds else "",
+            "title": issue["title"],
+            "url": issue["html_url"]
+        }
+        print " * **{bug_or_enh}**: [{title}]({url})".format(**values)
+    print ""
+
+
+gatekeepers = gh.GithubModel("/repos/UV-CDAT/uvcdat/issues?state=open&labels=Gatekeeper")
+
+if len(gatekeepers):
+    print "## OPEN GATEKEEPERS"
+    for issue in gatekeepers:
+        print " * [{title}]({url})".format(title=issue["title"], url=issue["html_url"])
+    print ""
+
 
 print "## Known Bugs\n"
 
 critical_issues = gh.GithubModel("/repos/UV-CDAT/uvcdat/issues?state=open&labels=critical")
 
 if len(critical_issues) > 0:
-	print "### Critical\n"
-	for issue in critical_issues:
-		print " * [{title}]({url})".format(title=issue["title"], url=issue["html_url"])
-	print ""
+    print "### Critical\n"
+    for issue in critical_issues:
+        print " * [{title}]({url})".format(title=issue["title"], url=issue["html_url"])
+    print ""
 
 high_issues = gh.GithubModel("/repos/UV-CDAT/uvcdat/issues?state=open&labels=high")
 if len(high_issues) > 0:
-	print "### High Priority\n"
-	for issue in high_issues:
-		print " * [{title}]({url})".format(title=issue["title"], url=issue["html_url"])
-	print ""
+    print "### High Priority\n"
+    for issue in high_issues:
+        print " * [{title}]({url})".format(title=issue["title"], url=issue["html_url"])
+    print ""
 
 
 if len(high_issues) == 0 and len(critical_issues) == 0:
-	print "No known issues!"
+    print "No known issues!"
--- init.py
+++ init.py
@@ -5,4 +5,4 @@ if sys.version_info < (2,7,9):
 	import urllib3.contrib.pyopenssl
 	urllib3.contrib.pyopenssl.inject_into_urllib3()
 
-gh.set_key(os.environ["GITHUB_KEY"])
+gh.set_key(os.environ["CHANGELOG_GITHUB_TOKEN"])
